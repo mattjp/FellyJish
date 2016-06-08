c3c5112b6bc2f5d5f55b0de2447891e866aa7949
mattjp <mattjp@umich.edu>
2016-06-08 18:52:51 -0400

    figured out syntax issues

--- fellyjish.js
+++ fellyjish.js
@@ -5,10 +5,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 
 function preload() {
 	game.load.image('jellard', 'img/jellard.png');
+	this.game.stage.scale.pageAlignHorizontally = true;
 }
 
 function create() {
 	game.add.sprite(0, 0, 'jellard');
+	game.add.sprite(100, 100, 'jellard');
 }
 
 function update() {
